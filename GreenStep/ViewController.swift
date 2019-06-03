90744660723ad4e3845f9586a6b451bfe8709eee
tomhuber <tomhuber@umich.edu>
2019-06-03 17:32:27 -0400

    Initial Commit

---
+++ GreenStep/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreenStep
+//
+//  Created by Thomas Huber on 6/3/19.
+//  Copyright © 2019 Thomas Huber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
